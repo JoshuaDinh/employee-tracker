47721e25baa162c76bc1869e9b667abbdf5462ad
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-24 13:00:51 -0700

    updated form component with basic markup and functionality to retrieve userinput and set to state

---
+++ client/src/Form/Form.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./form.css";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    age: "",
+    country: "",
+    salary: 0,
+  });
+
+  const { fullName, age, salary, country } = formData;
+
+  const updateFormData = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+  return (
+    <form className="form">
+      <h1>Enter Employee Info:</h1>
+      <label>Name:</label>
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={fullName}
+        onChange={(e) => updateFormData(e)}
+      />
+      <label>Age:</label>
+      <input
+        type="text"
+        placeholder="Age"
+        name="age"
+        value={age}
+        onChange={(e) => updateFormData(e)}
+      />
+      <label>Country:</label>
+      <input
+        type="text"
+        placeholder="Country"
+        name="country"
+        value={country}
+        onChange={(e) => updateFormData(e)}
+      />
+      <label>Salary:</label>
+      <input
+        type="number"
+        placeholder="Salary"
+        name="salary"
+        value={salary}
+        onChange={(e) => updateFormData(e)}
+      />
+      <button onClick={(event) => submit(event)}>Show Employee</button>
+    </form>
+  );
+};
+
+export default Form;
