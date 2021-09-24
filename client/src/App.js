a66f352ec25b5366210183675f2179e03e47b4ec
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-24 13:02:59 -0700

    removed extra

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./app.css";
+import EmployeeList from "./EmployeeList/EmployeeList";
+import Form from "./Form/Form";
 
 const App = () => {
-  return <div className="App">my app</div>;
+  return (
+    <div className="App">
+      <Form />
+      <EmployeeList />
+    </div>
+  );
 };
 
 export default App;
