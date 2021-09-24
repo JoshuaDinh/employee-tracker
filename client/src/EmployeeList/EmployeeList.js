4b8862d96413876ca32bfe45701ea6863e24e341
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-24 13:00:12 -0700

    added employeelist files

---
+++ client/src/EmployeeList/EmployeeList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./employeeList.css";
+
+const EmployeeList = () => {
+  return (
+    <div className="employeeList">
+      <h1>Employee List:</h1>
+    </div>
+  );
+};
+
+export default EmployeeList;
